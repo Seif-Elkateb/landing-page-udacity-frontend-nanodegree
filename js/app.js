ed0c7e4da6e6c64d3749915f21c689699b1faa30
sifelkateb <sifelkateb@gmail.com>
2021-09-06 23:43:19 +0200

    add global variables

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// begin global variables
+
+const sections = document.querySelectorAll('section'); // 
+const fragment= document.createDocumentFragment();
+const scrollToTopbtn=document.querySelector('#scrolltop');
+const pageFooter= document.querySelector('.page__footer');
+let timer=null;
+//end global variables
+
+

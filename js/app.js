80c0cc37813297b9490103910dd43fbb36eaa8bc
sifelkateb <sifelkateb@gmail.com>
2021-09-06 23:44:20 +0200

    add helper functions

--- js/app.js
+++ js/app.js
@@ -8,3 +8,24 @@ let timer=null;
 //end global variables
 
 
+//start helper functions 
+const createItems=()=>{
+  for (const section of sections){
+    const location=section.id;
+    const listName=section.dataset.nav;
+    const list=document.createElement('li');
+    const anchor=document.createElement('a');
+    anchor.textContent=listName;
+    anchor.href=`#${location}`;
+    anchor.id='section-'+location;
+    anchor.classList.add('menu__link');
+    list.appendChild(anchor);
+    fragment.appendChild(list);
+  }
+  }
+  const checkVisible=(element)=>{
+    const rect=element.getBoundingClientRect();
+    return rect.top<150 &&rect.bottom>150;
+  }
+  
+  //end helper functions
